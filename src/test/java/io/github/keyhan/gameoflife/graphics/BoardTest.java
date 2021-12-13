3b312377bd8f624e022ef739616eac334599833e
Keyhan Hadjari <keyhan.hadjari@gmail.com>
2021-12-13 20:07:36 +0100

    Make the test also OK for OSX.

--- src/test/java/io/github/keyhan/gameoflife/graphics/BoardTest.java
+++ src/test/java/io/github/keyhan/gameoflife/graphics/BoardTest.java
@@ -5,14 +5,15 @@ import org.junit.jupiter.api.Test;
 
 public class BoardTest {
 
+    private static final String OS = System.getProperty("os.name").toLowerCase();
+    public static final boolean IS_MAC = OS.contains("mac");
+    private static final String ORGANISM = IS_MAC ? "x" : new String(Character.toChars(0x1F37E));
+    private static final String OLD_ORGANISM = IS_MAC ? "X" : new String(Character.toChars(127877));
+    private static final String DEAD_ORGANISM = IS_MAC ? "." : new String(Character.toChars(0x1F32B));
+    private static final String EMPTY_CELL = " ";
+
     @Test
-    public void testConvertToStringTable() { 
-
-        String ORGANISM = new String(Character.toChars(0x1F37E));
-        final String OLD_ORGANISM = new String(Character.toChars(127877));
-        final String DEAD_ORGANISM = new String(Character.toChars(0x1F32B));
-
-
+    public void testConvertToStringTable() {
 
         int[][] initBord = new int[4][4];
         initBord[0] = new int[] {0, 0 ,0 ,0};
@@ -27,10 +28,10 @@ public class BoardTest {
         oldBord[3] = new int[] {0, 0 ,0 ,0};
 
         String[][] expectedBoard = new String[4][4];
-        expectedBoard[0] = new String[] {" ", " " ," " ," "};
-        expectedBoard[1] = new String[] {DEAD_ORGANISM, ORGANISM ,OLD_ORGANISM ," "};
-        expectedBoard[2] = new String[] {" ", OLD_ORGANISM ,ORGANISM ," "};
-        expectedBoard[3] = new String[] {" ", " " ," " ," "};
+        expectedBoard[0] = new String[] {EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL};
+        expectedBoard[1] = new String[] {DEAD_ORGANISM, ORGANISM ,OLD_ORGANISM ,EMPTY_CELL};
+        expectedBoard[2] = new String[] {EMPTY_CELL, OLD_ORGANISM ,ORGANISM ,EMPTY_CELL};
+        expectedBoard[3] = new String[] {EMPTY_CELL,EMPTY_CELL,EMPTY_CELL,EMPTY_CELL};
 
 
         Board board = new Board();
